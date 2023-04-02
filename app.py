1ff5de2376902a66ae4bc7fefee557595411bdec
mssonawane15 <122289941+mssonawane15@users.noreply.github.com>
2023-04-03 01:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def Predict():
     else:
          return render_template("result.html",data=f"Cutsomer will keep continue with your service" )
 
-   
-app.run(debug=True)
+if name == "__main__":   
+app.run(host = "0.0.0.0",port = "8080",debug=True)
